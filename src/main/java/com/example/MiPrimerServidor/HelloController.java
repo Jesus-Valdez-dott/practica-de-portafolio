2711e184b2a30b875f75548d344855c8f28e4014
Jesus-Valdez-dott <jesus.valdez253244@potros.itson.edu.mx>
2026-08-27 17:01:27 -0700

    practica de servidor springboot

---
+++ src/main/java/com/example/MiPrimerServidor/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.MiPrimerServidor;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {return "hola desde mi servidor de aplicaciones";}
+
+    @GetMapping("/saludo")
+    public String saludar(@RequestParam String nombre) { return "Hola: " + nombre + "desde el servidor de apps"; }
+
+    @PostMapping("/mensaje")
+    public String recibirmensaje(@RequestBody String mensaje) {return "recibi el mensaje";}
+}
